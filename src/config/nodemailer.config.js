a2523bb2b425d4426ca7fcf5c783bb726ad1ed65
Catalina Lima Brun <catalinalbrun@gmail.com>
2023-12-31 14:02:36 -0700

    first commit

---
+++ src/config/nodemailer.config.js
@@ -0,0 +1,8 @@
+export const mailSettings = {
+    service: 'gmail',
+    port: 587,
+    auth: {
+        user: 'cattlimm@gmail.com',
+        pass: '1234 5678 9123 4567'
+    }
+}
---
+++ src/config/swagger.config.js
@@ -0,0 +1,12 @@
+import  __dirname  from '../utils.js';
+
+export const swaggerOptions = {
+    definition: {
+        openapi: '3.0.1',
+        info: {
+            title:"API Docs",
+            description: "E-Commerce API Documentation &copy; Nits-Ar",
+        }
+    },
+    apis: [`${__dirname}/docs/**/*.yaml`]
+}
